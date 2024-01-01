19a9223c91fc7f8951dbad8232947b5fbaefd25f
Mohsen Tohidipour <tohidipor@gmail.com>
2024-01-01 08:03:39 +0330

    learn neon module-calc.js

---
+++ learn/neon/03-module-calc.js
@@ -0,0 +1,21 @@
+function addition (num1, num2) {
+    return num1 + num2;
+}
+
+function subtraction (num1, num2) {
+    return num1 - num2;
+}
+
+function multiplication (num1, num2) {
+    return num1 * num2;
+
+}
+
+function division (num1, num2) {
+     return num1 / num2;
+}
+
+console.log(addition(5, 2));
+console.log(subtraction(5, 2));
+console.log(multiplication(5, 2));
+console.log(division(5, 2));
