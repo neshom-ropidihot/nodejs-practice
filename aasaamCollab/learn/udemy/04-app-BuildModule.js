f9d8661bbcb953fc5cafe137c1c0926ef49fc385
Mohsen Tohidipour <tohidipor@gmail.com>
2024-01-29 08:56:40 +0330

    add 04-app-BuildModule and 04-greet-BuildModule in aasaamCollab

---
+++ aasaamCollab/learn/udemy/04-app-BuildModule.js
@@ -0,0 +1,9 @@
+//require('./04-greet-BuildModule');
+//greet();    // ReferenceError: greet is not defined
+
+
+
+var greet = require('./04-greet-BuildModule');
+greet();  
+
+// console.log(module);
---
+++ aasaamCollab/learn/udemy/04-greet-BuildModule.js
@@ -0,0 +1,16 @@
+// console.log("hi");
+// console.log(this);
+// console.log(module);
+
+
+
+// var greet = function() {
+    //     console.log("hi");
+// }
+
+
+var greet = function() {
+    console.log("hi");
+}
+
+module.exports = greet;
